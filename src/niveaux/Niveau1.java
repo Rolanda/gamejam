e995c9cd938fc3b42733200c46f5962a4db5e211
rolanda <arthur.roland@etudiant.univ-lille1.fr>
2015-06-16 17:10:05 +0200

    ajout de Detritus, et debut du niveau1 mais faut etre tolerant ^^

--- src/niveaux/Niveau1.java
+++ src/niveaux/Niveau1.java
@@ -1,11 +1,12 @@
 package niveaux;
 
 import java.awt.Dimension;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JFrame;
 
 import utils.Constante;
-import utils.Images;
 import Items.Detritus;
 /**
  * premier niveau
@@ -24,17 +25,56 @@ public class Niveau1 {
 		fenetre = new JFrame("Gamejam");
 		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		fenetre.setPreferredSize(new Dimension(Constante.ECRANX, Constante.ECRANY));
+		//fenetre.setLayout(null); // conard c toi la merde
 		
-		//fenetre.setLayout(null); connard
-		
-		
-		Detritus d1 = new Detritus(false, 50, 50);
-		System.out.println(Images.BLEU.getDescription());
+		Detritus d1 = new Detritus(true, 150, 150);
+		d1.repaint();
 		fenetre.getContentPane().add(d1);
+		
+		d1.addMouseListener(new MouseListener() {
+			
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+				// TODO Auto-generated method stub
 				
+			}
+			
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) 
+			{
+				if(e.getSource() instanceof Detritus){
+					Detritus d = (Detritus)e.getSource();
+					// TODO Auto-generated method stub
+					if(d.isAccesible())
+						d.setVisible(false);
+					else
+						System.out.println("lol");
+				}
+				
+			}
+		});
+		
 		fenetre.setResizable(false);
 		fenetre.pack();
-		fenetre.setVisible(true);	
+		fenetre.setVisible(true);
 	}
 	
 	/**
